3fb131f7f1cfb874fff990b2c696ef070ab77bf6
VictoriaTatarinova <chiarouomo@gmail.com>
2022-05-17 22:44:41 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 50_237;
+        boolean isRegistered = true;
+        int x = 20;
+
+        int bonus = amount / x;
+
+        System.out.println(bonus);
+
+    }
+}
